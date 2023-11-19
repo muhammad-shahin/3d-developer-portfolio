e20de60b3b5a4028a5e447edfd4c91c22d682a61
muhammad-shahin <muhammadshahin002@gmail.com>
2023-11-19 11:49:03 +0600

    technology i know section added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+import { motion } from 'framer-motion';
+import { textVariant } from '../utils/motion';
+import { styles } from '../styles';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div>
+      <motion.div
+        className='mb-8'
+        variants={textVariant()}
+      >
+        <p className={`${styles.sectionSubText} text-center`}>
+          technology I work with
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>TECHNOLOGY</h2>
+      </motion.div>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div
+            className='w-28 h-28'
+            key={technology.name}
+          >
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, '');
